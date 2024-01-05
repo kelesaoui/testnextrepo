8ebdd7e22a38c8556e8614cb0557105e1f1216c7
kelesaoui <khalidelesaoui@yahoo.fr>
2024-01-05 11:17:56 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental:{appDir: true},
-     output:"standalone"
+    experimental:{appDir: true}
 }
 
 module.exports = nextConfig
