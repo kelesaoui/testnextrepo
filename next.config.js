4d9e4937b8e510a5821a3c90a90e540cb18e7960
kelesaoui <khalidelesaoui@yahoo.fr>
2024-01-05 13:45:51 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental:{appDir: true}
+    experimental:{appDir: true},
+      images: {
+    remotePatterns: [
+      {
+        hostname: 'auseaacmpstorageaccount.blob.core.windows.net',
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
